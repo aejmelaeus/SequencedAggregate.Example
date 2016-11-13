42cf03225e99427eb6f747b76a5b7857de41b9bf
aejmelaeus <weikko.aejmelaeus@gmail.com>
2016-11-13 14:42:07 +0100

    Some boilerplate structure and thoughts

---
+++ src/Payroll.Domain/Interfaces/IPayrollRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Payroll.Domain.Interfaces
+{
+    public interface IPayrollRepository
+    {
+        PayrollCustomer GetCustomerById(string id);
+        void Commit(PayrollCustomer payrollCustomer);
+        PayrollUser GetUserById(string id);
+    }
+}
