be8442fae7a8c1e65b4411c9a06172eb687315b4
Weikko Aejmelaeus <waejmelaeu002@pwc.se>
2016-11-15 06:06:11 +0100

    Updated the PayrollUser

--- src/Payroll.Domain/PayrollUser.cs
+++ src/Payroll.Domain/PayrollUser.cs
@@ -10,10 +10,6 @@ namespace Payroll.Domain
 
         public Guid Id { get; }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-
         public IEnumerable<string> CustomerIds => _customerIds;
         
         public PayrollUser(Guid id)
@@ -22,27 +18,16 @@ namespace Payroll.Domain
 
             RegisterTransition<UserAddedToCustomer>(Transition);
             RegisterTransition<UserRemovedFromCustomer>(Transition);
-            RegisterTransition<UserUpdated>(Transition);
         }
 
         private void Transition(UserAddedToCustomer @event)
         {
-            FirstName = @event.FirstName;
-            LastName = @event.LastName;
-            Email = @event.Email;
             _customerIds.Add(@event.CustomerId);
         }
 
         private void Transition(UserRemovedFromCustomer @event)
         {
             _customerIds.Remove(@event.CustomerId);
-        }
-
-        private void Transition(UserUpdated @event)
-        {
-            FirstName = @event.FirstName;
-            LastName = @event.LastName;
-            Email = @event.Email;
         }
     }
 }
--- src/Payroll.Endpoint/Handlers/UserUpdatedHandler.cs
+++ src/Payroll.Endpoint/Handlers/UserUpdatedHandler.cs
@@ -21,20 +21,18 @@ namespace Payroll.Endpoint.Handlers
             var sequenceAnchor = message.TimeStamp.Ticks;
             var messageId = Guid.Parse(context.MessageId);
             var userUpdatedEvent = new Domain.Events.UserUpdated(message.FirstName, message.LastName, message.Email);
-            
-            _payrollRepository.CommitEvent(id.ToString(), sequenceAnchor, messageId, userUpdatedEvent );
 
             var user = _payrollRepository.GetUserById(id);
 
             foreach (var customerId in user.CustomerIds)
             {
-                _payrollRepository.CommitEvent(customerId, sequenceAnchor, Guid.NewGuid(), userUpdatedEvent);
+                _payrollRepository.CommitEvent(customerId, sequenceAnchor, messageId, userUpdatedEvent);
                 context.Send(new SynchronizePayrollCustomer
                 {
                     Id = customerId
                 });
             }
-        
+
             return Task.FromResult(0);
         }
     }
